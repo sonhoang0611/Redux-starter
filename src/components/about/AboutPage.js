a1371173831ee62a46b7399dea141dbdce8fc9ae
Son Hoang <hoangthaison985@gmail.com>
2017-11-23 05:35:06 +0700

    Initial app structure

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,21 @@
+
+// Since the about page needs no data or actions, this doesn't
+// contain the usual Redux container boilerplate
+// for mapState and mapDispatch.
+import React from 'react';
+
+// it's not a stateless functional comp because there's a limitation in hot reloading
+// because stateless functional comp aren't hot reloaded unless they have some parent
+// that is a class
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This application uses React, Redux, React Router and a variety of other helpful libraries.</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
